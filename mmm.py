96f1f09ec670b42fe4bb32334c48c0c3e385cd42
Мельникова Маргарита Валерьевна <margarita.melnikova@ylab.io>
2025-03-01 23:18:12 +0300

    first commit

---
+++ mmm.py
@@ -0,0 +1,14 @@
+import re
+import time
+import urllib2
+
+url = "http://lenta.ru/"
+content = urllib2.urlopen(url).read()
+imgUrls = re.findall('img .*?src="(.*?)"', сontent)
+
+start = time.time()
+for img in imgUrls:
+    if img.endswith(".jpg"):
+        """реализация метода по загрузке изображения из url"""
+
+# print time.time()-start
