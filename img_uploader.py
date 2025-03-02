bd70e25b534cf80858c53b2eed00ad2e180b20b7
Мельникова Маргарита Валерьевна <margarita.melnikova@ylab.io>
2025-03-02 10:56:05 +0300

    added comments

--- img_uploader.py
+++ img_uploader.py
@@ -9,15 +9,17 @@ from parse_html import get_album_name
 
 
 def main(html_path, target_base_folder):
+    """ Получение фото с html- страницы и сохранение в директорию по названию альбома """
     response = urllib.request.urlopen(f"file:///{html_path}") # для винды
+    # Получаем список ссылок на изображения
     html = response.read()
     img_urls = re.findall('img .*?src="(.*?)"', str(html))
 
     target_folder = get_album_name(html)
-
+    # Создаем директорию по имени альбома, куда будем загружать фото
     os.makedirs(os.path.join(target_base_folder, target_folder), exist_ok=True)
     index = 0
-
+    # Загрузка фото
     for img in img_urls:
         if img.startswith("https"):
             file_path = os.path.join(target_base_folder, target_folder, f"{index}.jpg")
@@ -29,6 +31,7 @@ def main(html_path, target_base_folder):
 if __name__ == "__main__":
     archive_folder = vars(parse_arguments())["in"]
     target_base_folder = vars(parse_arguments())["out"] or config.BASE_FOLDER
+    # Получаем список всех html из архивной папки (каждый html - это альбом с фото)
     files = sorted(f for f in os.listdir(archive_folder) if f.endswith(".html"))
     for file in files:
         html_path = os.path.abspath(os.path.join(archive_folder, file))
--- parse_html.py
+++ parse_html.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 
 def get_album_name(html):
+    """ Парсим название альбома с html-страницы """
     soup = BeautifulSoup(html, "html.parser")
     album_div = soup.find("div", class_="ui_crumb")
     album_name_ = album_div.get_text(strip=True)
